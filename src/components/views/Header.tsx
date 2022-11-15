a32ecde17f0b01e817578b5405fc1fa685371920
Minna Salonen <minsalon@Minnas-MBP.home>
2022-11-15 11:23:36 +0200

    added initial locale support

--- src/components/views/Header.tsx
+++ src/components/views/Header.tsx
@@ -8,6 +8,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "@Store/store";
 import Button from "@Components/tools/Button";
 import { toggle } from "@Store/dataSlices";
+import { useTranslation } from "react-i18next";
+import DropDown, { ListItem } from "@Components/tools/DropDown";
 
 const Header = (): ReactElement => {
   const selectedTheme = useSelector((state: RootState) => state.theme);
@@ -15,9 +17,22 @@ const Header = (): ReactElement => {
   const theme = useTheme<Itheme>();
   const styles = useHeaderStyles(theme);
   const titleStyles = useTitleStyles(theme);
+  const { t, i18n } = useTranslation();
+  const locales = ["en", "fi"];
   const toggleTheme = () => {
     dispatch(toggle());
   };
+
+  const langItems = locales.map(
+    (lang: string) =>
+      ({
+        value: lang,
+        label: lang.toLocaleUpperCase(),
+        callback: () => {
+          i18n.changeLanguage(lang);
+        },
+      } as ListItem)
+  );
 
   return (
     <div className={styles.header}>
@@ -25,13 +40,14 @@ const Header = (): ReactElement => {
         icon={faRainbow}
         className={titleStyles.title + " " + titleStyles.icon}
       />
-      <h1 className={titleStyles.title}>Small Demo with React + Vite + TS</h1>
+      <h1 className={titleStyles.title}>{t("app-title")}</h1>
       <Button
         icon={selectedTheme.theme === "light" ? faMoon : faSun}
         border="none"
         padding="0"
         onClick={toggleTheme}
       />
+      <DropDown selected={langItems.at(0)?.label} items={langItems} />
     </div>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import i18n from "i18next";
 import "./index.css";
-
+import { initReactI18next } from "react-i18next";
+import en from "@Assets/lang/en-US.json";
+import fi from "@Assets/lang/fi-FI.json";
+i18n.use(initReactI18next).init({
+  resources: {
+    en: {
+      translation: en,
+    },
+    fi: {
+      translation: fi,
+    },
+  },
+  lng: "en",
+  fallbackLng: "en",
+  interpolation: {
+    escapeValue: false,
+  },
+});
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
